c9eb324a50d2140a578451b93badd1ca3707f05a
Kunal <kunalsingh95999@gmail.com>
2026-05-23 13:43:01 +0530

    Day 1: Spring Boot skeleton setup, package structure, clean pom.xml with WebClient

---
+++ src/main/java/com/ailens/AiLensApplication.java
@@ -0,0 +1,13 @@
+package com.ailens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ailens/ai_lens/AiLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ailens.ai_lens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
